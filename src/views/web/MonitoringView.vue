bc553905af96a7f8fd18439bc7b6b6d275762355
Sanetuk <yng.uk.kim@gmail.com>
2024-10-07 12:17:48 +0900

    Design: 카카오맵 포맷 변경

--- src/views/web/MonitoringView.vue
+++ src/views/web/MonitoringView.vue
@@ -2,18 +2,19 @@
 import { onMounted } from 'vue';
 import MainSidebar from '@/components/main/MainSidebar.vue';
 
+// 카카오맵 스크립트 로드
 const loadScript = () => {
   return new Promise((resolve) => {
+    // 카카오맵이 로드되었는지 확인함
     if (window.kakao && window.kakao.maps) {
-      // If Kakao Maps is already loaded, resolve immediately
       resolve();
     } else {
-      // Dynamically create a script element for Kakao Maps
+      // 카카오맵이 로드되지 않은 경우 새로운 스크립트 생성
       const script = document.createElement('script');
       script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=a4128381b093c4d13c6d256b2bd39303&autoload=false`;
       script.onload = () => {
         window.kakao.maps.load(() => {
-          resolve(); // Resolve the promise once Kakao Maps is loaded
+          resolve();
         });
       };
       document.head.appendChild(script);
@@ -21,17 +22,19 @@ const loadScript = () => {
   });
 };
 
+// 카카오맵 지도 로드
 const loadMap = () => {
-  const container = document.getElementById('map'); // The map container
+  const container = document.getElementById('map'); // 지도 객체
   const options = {
-    center: new window.kakao.maps.LatLng(33.450701, 126.570667), // Map's center coordinates
-    level: 3, // Map's zoom level
+    center: new window.kakao.maps.LatLng(36.5, 127.5), // 지도 첫 화면 좌표
+    level: 12, // 지도 줌 배율
   };
 
-  // Create and display the map
+  // 지도 만들기
   new window.kakao.maps.Map(container, options);
 };
 
+// 페이지가 로드될 때 스크립트와 지도 초기화
 onMounted(async () => {
   await loadScript(); // Load the Kakao Map script
   loadMap(); // Initialize the map
@@ -39,30 +42,25 @@ onMounted(async () => {
 </script>
 
 <template>
-  <KakaoMap />
   <div id="mainPage" class="d-flex flex-column">
-    <div class="app-wrapper flex-column flex-row-fluid" id="kt_app_wrapper">
+    <div class="flex-column flex-row-fluid" id="kt_app_wrapper">
       <!-- Sidebar Component -->
       <MainSidebar />
 
       <!-- Main Content -->
       <div class="app-main flex-column flex-row-fluid" id="kt_app_main">
-        <RouterView id="bodyPart" class="flex-grow-1 p-0" />
-        <h1>전체현황 테스트중입니당</h1>
 
-        <!-- Kakao Map Container -->
+        <!-- Begin::Kakao Map -->
         <div>
-          <h2>카카오 맵 보기</h2>
-          <div id="map"></div>
+          <KakaoMap />
+          <div id="map" class="mt-0"></div>
         </div>
+        <!-- end::Kakao Map -->
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-#map {
-  width: 100%;
-  height: 400px;
-}
+
 </style>
